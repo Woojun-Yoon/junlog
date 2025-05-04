5c4a15e62978c1a1cec5806c535ef94570a0e1c7
Woojun-Yoon <dbsdnwns11@naver.com>
2025-05-05 07:29:12 +0900

    feat: Admin panel Monaco 코드 블럭 수정

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -1390,7 +1390,21 @@ export interface BannerBlock {
  * via the `definition` "CodeBlock".
  */
 export interface CodeBlock {
-  language?: ('typescript' | 'javascript' | 'css') | null;
+  language?:
+    | (
+        | 'java'
+        | 'python'
+        | 'kotlin'
+        | 'groovy'
+        | 'bash'
+        | 'yaml'
+        | 'typescript'
+        | 'javascript'
+        | 'css'
+        | 'html'
+        | 'json'
+      )
+    | null;
   code: string;
   id?: string | null;
   blockName?: string | null;
--- src/payload/blocks/Code/config.ts
+++ src/payload/blocks/Code/config.ts
@@ -1,33 +1,95 @@
-import type { Block } from 'payload'
+import type { Block } from "payload";
 
 export const Code: Block = {
-  slug: 'code',
-  interfaceName: 'CodeBlock',
+  slug: "code",
+  interfaceName: "CodeBlock",
   fields: [
     {
-      name: 'language',
-      type: 'select',
-      defaultValue: 'typescript',
+      name: "language",
+      type: "select",
+      defaultValue: "java",
       options: [
         {
-          label: 'Typescript',
-          value: 'typescript',
+          label: "Java",
+          value: "java",
         },
         {
-          label: 'Javascript',
-          value: 'javascript',
+          label: "Python",
+          value: "python",
         },
         {
-          label: 'CSS',
-          value: 'css',
+          label: "Kotlin",
+          value: "kotlin",
+        },
+        {
+          label: "Groovy",
+          value: "groovy",
+        },
+        {
+          label: "Bash",
+          value: "bash",
+        },
+        {
+          label: "YAML",
+          value: "yaml",
+        },
+        {
+          label: "Typescript",
+          value: "typescript",
+        },
+        {
+          label: "Javascript",
+          value: "javascript",
+        },
+        {
+          label: "CSS",
+          value: "css",
+        },
+        {
+          label: "HTML",
+          value: "html",
+        },
+        {
+          label: "JSON",
+          value: "json",
         },
       ],
     },
     {
-      name: 'code',
-      type: 'code',
+      name: "code",
+      type: "code",
       label: false,
       required: true,
+      admin: {
+        editorOptions: {
+          language: "plaintext",
+          automaticLayout: true,
+          fontFamily: "monospace",
+          fontSize: 16,
+          lineHeight: 24,
+          wordWrap: "off",
+          renderWhitespace: "none",
+          insertSpaces: false,
+          tabSize: 4,
+          theme: "vs-dark",
+          lineNumbers: "on",
+          scrollBeyondLastLine: false,
+          minimap: { enabled: false },
+          padding: { top: 10, bottom: 10 },
+          scrollbar: {
+            vertical: "visible",
+          },
+
+          // disable all suggestions
+          suggestOnTriggerCharacters: false,
+          quickSuggestions: false,
+          wordBasedSuggestions: "off",
+          parameterHints: {
+            enabled: false,
+          },
+          renderValidationDecorations: "off",
+        },
+      },
     },
   ],
-}
+};
