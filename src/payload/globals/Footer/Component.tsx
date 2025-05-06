45a53e51a2dfb7d586d6771aedf8939d2ce532d4
Woojun-Yoon <dbsdnwns11@naver.com>
2025-05-07 05:35:29 +0900

    fix: Header, Footer 수정

--- src/payload/globals/Footer/Component.tsx
+++ src/payload/globals/Footer/Component.tsx
@@ -1,35 +1,37 @@
-import { getCachedGlobal } from '@/lib/utils/getGlobals'
-import Link from 'next/link'
-import React from 'react'
+import { getCachedGlobal } from "@/lib/utils/getGlobals";
+import Link from "next/link";
+import React from "react";
 
-import type { Footer } from '@/payload-types'
+import type { Footer } from "@/payload-types";
 
-import { ThemeSelector } from '@/components/ThemeProvider/Theme/ThemeSelector'
-import { CMSLink } from '@/payload/fields/Link/index'
-import { Logo } from '@/components/Logo/Logo'
+import { CMSLink } from "@/payload/fields/Link/index";
+import { Logo } from "@/components/Logo/Logo";
+import { Lock } from "lucide-react";
 
 export async function Footer() {
-  const footerData = await getCachedGlobal('footer', 1)() as Footer
+  const footerData = (await getCachedGlobal("footer", 1)()) as Footer;
 
-  const navItems = footerData?.navItems || []
+  const navItems = footerData?.navItems || [];
 
   return (
     <footer className="mt-auto border-t border-border bg-black dark:bg-card text-white">
-      <div className="container py-8 gap-8 flex flex-col md:flex-row md:justify-between">
+      <div className="container py-8 gap-8 flex flex-row justify-between">
         <Link className="flex items-center" href="/">
           <Logo />
         </Link>
 
-        <div className="flex flex-col-reverse items-start md:flex-row gap-4 md:items-center">
-          <ThemeSelector />
-          <CMSLink label='Admin' className="text-white" url={'/admin'} newTab />
-          <nav className="flex flex-col md:flex-row gap-4">
+        <div className="flex flex-row flex-wrap items-center gap-4">
+          <CMSLink url={"/admin"} newTab>
+            <Lock className="h-4 w-4" />
+          </CMSLink>
+
+          <nav className="flex flex-wrap gap-4">
             {navItems.map(({ link }, i) => {
-              return <CMSLink className="text-white" key={i} {...link} />
+              return <CMSLink className="text-white" key={i} {...link} />;
             })}
           </nav>
         </div>
       </div>
     </footer>
-  )
+  );
 }
--- src/payload/globals/Header/Component.client.tsx
+++ src/payload/globals/Header/Component.client.tsx
@@ -8,6 +8,7 @@ import type { Header } from "@/payload-types";
 
 import { Logo } from "@/components/Logo/Logo";
 import { HeaderNav } from "./Nav";
+import { ThemeSelector } from "@/components/ThemeProvider/Theme/ThemeSelector";
 
 interface HeaderClientProps {
   data: Header;
@@ -31,17 +32,23 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
 
   return (
     <header
-      className="container relative z-20   "
+      className="container relative z-20"
       {...(theme ? { "data-theme": theme } : {})}
     >
-      <div className="py-6 flex justify-between">
-        <Link href="/">
-          <Logo
-            loading="eager"
-            priority="high"
-            className="invert dark:invert-0"
-          />
-        </Link>
+      <div className="py-6 flex flex-col gap-y-4 sm:flex-row sm:justify-between sm:items-center">
+        <div className="flex items-center justify-between w-full sm:w-auto">
+          <Link href="/">
+            <Logo
+              loading="eager"
+              priority="high"
+              className="invert dark:invert-0"
+            />
+          </Link>
+          <div className="ml-4 sm:ml-6">
+            <ThemeSelector />
+          </div>
+        </div>
+
         <HeaderNav data={data} />
       </div>
     </header>
