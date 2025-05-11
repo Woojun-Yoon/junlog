ca32039490ffbea437ff8f03abe310c6a0a265da
Woojun-Yoon <dbsdnwns11@naver.com>
2025-05-12 03:04:30 +0900

    fix: Favicon 경로 지정 및 파일 추가

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -11,14 +11,13 @@ import { mergeOpenGraph } from "@/lib/utils/mergeOpenGraph";
 import { getServerSideURL } from "@/lib/utils/getURL";
 import "./globals.css";
 import { Providers } from "./providers";
+import InitFavicon from "@/components/ThemeProvider/Theme/InitFavicon";
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const basePath = process.env.BASE_PATH || "";
-
   return (
     <html
       className={`${Pretendard.className}`}
@@ -32,29 +31,7 @@ export default async function RootLayout({
           media="(prefers-color-scheme: dark)"
         />
         <InitTheme />
-        <link
-          rel="apple-touch-icon"
-          sizes="76x76"
-          href={`${basePath}/favicons/apple-touch-icon.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href={`${basePath}/favicons/favicon-32x32.png`}
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href={`${basePath}/favicons/favicon-16x16.png`}
-        />
-        <link rel="manifest" href={`${basePath}/favicons/site.webmanifest`} />
-        <link
-          rel="mask-icon"
-          href={`${basePath}/favicons/safari-pinned-tab.svg`}
-          color="#5bbad5"
-        />
+        <InitFavicon />
       </head>
       <body>
         <Providers>
