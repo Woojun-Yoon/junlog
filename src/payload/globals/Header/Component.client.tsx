7aae72f96299ddc5e864e4a91c105c617e80607e
Woojun-Yoon <dbsdnwns11@naver.com>
2025-05-18 04:59:20 +0900

    fix: hydration 문제 해결

--- src/payload/globals/Header/Component.client.tsx
+++ src/payload/globals/Header/Component.client.tsx
@@ -15,19 +15,19 @@ interface HeaderClientProps {
 }
 
 export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
-  /* Storing the value in a useState to avoid hydration errors */
-  const [theme, setTheme] = useState<string | null>(null);
   const { headerTheme, setHeaderTheme } = useHeaderTheme();
   const pathname = usePathname();
 
-  useEffect(() => {
-    setHeaderTheme(null);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pathname]);
+  const [theme, setTheme] = useState<string | null>(null);
 
   useEffect(() => {
-    if (headerTheme && headerTheme !== theme) setTheme(headerTheme);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    setHeaderTheme(null);
+  }, [pathname, setHeaderTheme]);
+
+  useEffect(() => {
+    if (headerTheme) {
+      setTheme(headerTheme);
+    }
   }, [headerTheme]);
 
   return (
@@ -48,7 +48,6 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
             <ThemeSelector />
           </div>
         </div>
-
         <HeaderNav data={data} />
       </div>
     </header>
