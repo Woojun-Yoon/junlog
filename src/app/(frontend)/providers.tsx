f4e5becbc01bdf4a0b092b564ef980c5fbd48e97
Woojun-Yoon <dbsdnwns11@naver.com>
2025-10-22 05:17:58 +0900

    remove: posthog 제거

--- src/app/(frontend)/providers.tsx
+++ src/app/(frontend)/providers.tsx
@@ -1,16 +1,13 @@
 import { HeaderThemeProvider } from "@/components/ThemeProvider/HeaderTheme";
 import { ThemeProvider } from "@/components/ThemeProvider/Theme";
-import { PostHog } from "@/components/PostHog";
 import React from "react";
 
 export const Providers: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
   return (
-    <PostHog>
-      <ThemeProvider>
-        <HeaderThemeProvider>{children}</HeaderThemeProvider>
-      </ThemeProvider>
-    </PostHog>
+    <ThemeProvider>
+      <HeaderThemeProvider>{children}</HeaderThemeProvider>
+    </ThemeProvider>
   );
 };
